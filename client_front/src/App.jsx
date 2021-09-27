ee664100c93d10538361be2994f46c2e0ce8199f
eldos27 <bakaeveldos3@gmail.com>
2021-09-27 13:28:15 +0600

    Fronted ready

---
+++ client_front/src/App.jsx
@@ -0,0 +1,10 @@
+import "./app.scss";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
