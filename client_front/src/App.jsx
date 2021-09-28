4b1bb6e3bb0c2646e42a194592d2e01c01c14bf0
eldos27 <bakaeveldos3@gmail.com>
2021-09-28 10:50:36 +0600

    api ready

--- client_front/src/App.jsx
+++ client_front/src/App.jsx
@@ -1,10 +1,44 @@
 import "./app.scss";
 import Login from "./pages/login/Login";
+import Watch from "./pages/watch/Watch";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 function App() {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route exact path="/register">
+          {user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route exact path="/login">
+          {user ? <Login /> : <Redirect to="/" />}
+        </Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movie" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- client_front/src/pages/home/Home.jsx
+++ client_front/src/pages/home/Home.jsx
@@ -2,16 +2,42 @@ import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import "./home.scss";
 import List from "../../components/list/List";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNTE5YTNkOGVhY2E2Yzk3MTVmNGMwZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMjc3MTk5OSwiZXhwIjoxNjMzMjAzOTk5fQ.VyNiyO6eAUhoyF7h04uoNhTDORDL9k5zWY97bkPs3uA",
+            },
+          }
+        );
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
-      <Featured />
-      <List />
-      <List />
-      <List />
-      <List />
+      <Featured type={type} setGenre={setGenre} />
+      {lists.map((list) => (
+        <List list={list} />
+      ))}
     </div>
   );
 };
